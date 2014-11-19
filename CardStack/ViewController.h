a8a18c8b6ff81cf34898409acc4fe3bc30085aec
Vitaly Berg <bergusman@gmail.com>
2014-11-19 23:09:46 +0300

    Initial Commit

---
+++ CardStack/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CardStack
+//
+//  Created by Vitaly Berg on 19/11/14.
+//  Copyright (c) 2014 Vitaly Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CardStack/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CardStack
+//
+//  Created by Vitaly Berg on 19/11/14.
+//  Copyright (c) 2014 Vitaly Berg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
